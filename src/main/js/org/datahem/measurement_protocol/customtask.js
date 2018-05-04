c327a5cbcba46a59ae092d53a99bcbd73461a66c
Robert Sahlin <robert.sahlin@mathem.se>
2018-05-04 11:45:35 +0200

    changed tracking script to support multiple local collector endpoints

--- src/main/js/org/datahem/measurement_protocol/customtask.js
+++ src/main/js/org/datahem/measurement_protocol/customtask.js
@@ -1,12 +1,6 @@
 function() {
-  	// Add your collector endpoint here
-  	//var endpoint = 'https://myprojectid.appspot.com/collect/';
-  var endpoint = {{datahem collector endpoint}};
-	
 	return function(model) {
-		//add trackingId to collector path -> https://myprojectid/collect/UA-1234567-89/
-      var path = ((endpoint.substr(-1) !== '/') ? endpoint + '/' : endpoint) + model.get('trackingId') + '/';
-	    
+
 	    var globalSendTaskName = '_' + model.get('trackingId') + '_sendHitTask';
 	    
 	    var originalSendHitTask = window[globalSendTaskName] = window[globalSendTaskName] || model.get('sendHitTask');
@@ -14,25 +8,34 @@ function() {
 	    model.set('sendHitTask', function(sendModel) {
 	    	var payload = sendModel.get('hitPayload');
 	      	originalSendHitTask(sendModel);  
-	      	if (navigator.sendBeacon) {
-	      		navigator.sendBeacon(path, payload);
-	  	  	}
-	  	  	else if (typeof new XMLHttpRequest().responseType === 'string') {
-	      		var request = new XMLHttpRequest();
-	        	request.open('POST', path, true);
-	        	request.setRequestHeader('Content-type', 'text/plain; charset=UTF-8');
-	        	request.send(payload);
-	  		} 
-	  		else {
-	    		var beacon = document.createElement("img");
-	     		if( beacon.onerror ){
-	     			beacon.onerror = function(){}; 
-	     		}
-	        	if( beacon.onload ){
-	        		beacon.onload  = function(){}; 
-	        	}
-	     		beacon.src = path + '.gif?' + payload;
-	    	}     
+
+			var endpoints = {{datahem collector endpoints}};
+	      	var i, len, endpointsArr = endpoints.split(",");
+			for (len = endpointsArr.length, i=0; i<len; ++i) {
+	      		var endpoint = endpointsArr[i];
+	      		//add trackingId to collector path -> https://myprojectid/collect/UA-1234567-89/
+	      		var path = ((endpoint.substr(-1) !== '/') ? endpoint + '/' : endpoint) + model.get('trackingId') + '/';
+		      	if (navigator.sendBeacon) {
+		      		navigator.sendBeacon(path, payload);
+		  	  	}
+		  	  	else if (typeof new XMLHttpRequest().responseType === 'string') {
+		      		var request = new XMLHttpRequest();
+		        	request.open('POST', path, true);
+		        	request.setRequestHeader('Content-type', 'text/plain; charset=UTF-8');
+		        	request.send(payload);
+		  		} 
+		  		else {
+		    		var beacon = document.createElement("img");
+		     		if( beacon.onerror ){
+		     			beacon.onerror = function(){}; 
+		     		}
+		        	if( beacon.onload ){
+		        		beacon.onload  = function(){}; 
+		        	}
+		     		beacon.src = path + '.gif?' + payload;
+		    	}
+			}
+
 		});
 	};
 }
